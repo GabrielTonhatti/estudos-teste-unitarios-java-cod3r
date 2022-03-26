967206b3c57430faa8e13c0f97859cbf29cb6093
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-03-26 00:03:49 -0300

    Tratamento de exceções

--- src/main/java/br/com/gabrieltonhatti/servicos/LocacaoService.java
+++ src/main/java/br/com/gabrieltonhatti/servicos/LocacaoService.java
@@ -9,23 +9,27 @@ import br.com.gabrieltonhatti.entidades.Locacao;
 import br.com.gabrieltonhatti.entidades.Usuario;
 
 public class LocacaoService {
-	
-	public Locacao alugarFilme(Usuario usuario, Filme filme) {
-		Locacao locacao = new Locacao();
-		locacao.setFilme(filme);
-		locacao.setUsuario(usuario);
-		locacao.setDataLocacao(new Date());
-		locacao.setValor(filme.getPrecoLocacao());
 
-		// Entrega no dia seguinte
-		Date dataEntrega = new Date();
-		dataEntrega = adicionarDias(dataEntrega, 1);
-		locacao.setDataRetorno(dataEntrega);
+    public Locacao alugarFilme(Usuario usuario, Filme filme) throws Exception {
+        if (filme.getEstoque() == 0) {
+            throw new Exception("Filme sem estoque");
+        }
 
-		// Salvando a locacao...
-		// TODO adicionar método para salvar
+        Locacao locacao = new Locacao();
+        locacao.setFilme(filme);
+        locacao.setUsuario(usuario);
+        locacao.setDataLocacao(new Date());
+        locacao.setValor(filme.getPrecoLocacao());
 
-		return locacao;
-	}
+        // Entrega no dia seguinte
+        Date dataEntrega = new Date();
+        dataEntrega = adicionarDias(dataEntrega, 1);
+        locacao.setDataRetorno(dataEntrega);
+
+        // Salvando a locacao...
+        // TODO adicionar método para salvar
+
+        return locacao;
+    }
 
 }
--- src/test/java/br/com/gabrieltonhatti/service/LocacaoServiceTest.java
+++ src/test/java/br/com/gabrieltonhatti/service/LocacaoServiceTest.java
@@ -4,21 +4,33 @@ import br.com.gabrieltonhatti.entidades.Filme;
 import br.com.gabrieltonhatti.entidades.Locacao;
 import br.com.gabrieltonhatti.entidades.Usuario;
 import br.com.gabrieltonhatti.servicos.LocacaoService;
-import br.com.gabrieltonhatti.utils.DataUtils;
+import org.junit.Rule;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ErrorCollector;
+import org.junit.rules.ExpectedException;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Date;
 
 import static br.com.gabrieltonhatti.utils.DataUtils.isMesmaData;
-import static org.hamcrest.CoreMatchers.*;
+import static br.com.gabrieltonhatti.utils.DataUtils.obterDataComDiferencaDias;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 public class LocacaoServiceTest {
 
+    @Rule
+    public ErrorCollector error = new ErrorCollector();
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
     @Test
-    public void teste() {
+    public void testeLocacao() throws Exception {
         // Cenário
         LocacaoService service = new LocacaoService();
         Usuario usuario = new Usuario("Usuário 1");
@@ -28,12 +40,53 @@ public class LocacaoServiceTest {
         Locacao locacao = service.alugarFilme(usuario, filme);
 
         // Verificação
+        error.checkThat(locacao.getValor(), is(equalTo(6.0)));
+        error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+        error.checkThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(false));
+    }
 
-        // Equivalente ao Assert.assertThat do JUnit 4
-        assertThat(locacao.getValor(), is(equalTo(5.0)));
-        assertThat(locacao.getValor(), is(not(6.0)));
-        assertThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
-        assertThat(isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
+    @Test
+    public void testLocacao_fimeSemEstoque() throws Exception {
+        // Equivalente ao @Test(excepted = Exception.class) do org.junit.Test
+        assertThrows(Exception.class, () -> {
+            // Cenário
+            LocacaoService service = new LocacaoService();
+            Usuario usuario = new Usuario("Usuário 1");
+            Filme filme = new Filme("Filme 1", 0, 5.0);
+
+            // Ação
+            service.alugarFilme(usuario, filme);
+        });
+    }
+
+    @Test
+    public void testLocacao_fimeSemEstoque_2() {
+        // Cenário
+        LocacaoService service = new LocacaoService();
+        Usuario usuario = new Usuario("Usuário 1");
+        Filme filme = new Filme("Filme 1", 0, 5.0);
+
+        // Ação
+        try {
+            service.alugarFilme(usuario, filme);
+            Assertions.fail("Deveria ter lançado uma exceção");
+        } catch (Exception e) {
+            assertThat(e.getMessage(), is("Filme sem estoque"));
+        }
+    }
+
+    @org.junit.Test
+    public void testLocacao_fimeSemEstoque_3() throws Exception {
+            // Cenário
+            LocacaoService service = new LocacaoService();
+            Usuario usuario = new Usuario("Usuário 1");
+            Filme filme = new Filme("Filme 1", 0, 5.0);
+
+            exception.expect(Exception.class);
+            exception.expectMessage("Filme sem estoque");
+
+            // Ação
+            service.alugarFilme(usuario, filme);
 
     }
 
